602c2069fcb80c370001a087ba3424e5645900c8
jenya <hypnagonia@gmail.com>
2020-10-30 20:27:14 +0300

    earn page

--- src/pages/Earn/Manage.tsx
+++ src/pages/Earn/Manage.tsx
@@ -1,3 +1,4 @@
+/*
 import React, { useCallback, useState } from 'react'
 import { AutoColumn } from '../../components/Column'
 import styled from 'styled-components'
@@ -338,4 +339,6 @@ export default function Manage({
       </PositionInfo>
     </PageWrapper>
   )
-}
+}*/
+
+export {}
